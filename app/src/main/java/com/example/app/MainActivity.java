844cf6817c9aa2906fc427dc3b15fe89eda44602
Andre.v2005@aluno.ifsc.edu.br <andre.v2005@aluno.ifsc.edu.br>
2025-09-01 11:54:44 -0300

    interface criada

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.app;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,37 +12,39 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
+    Button b;
+    int i =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        b=findViewById(R.id.button);
+        tv=findViewById(R.id.textView);
+        tv.setText("blá");
+        b.setText("sorteardxcfvgbhnm");
+       //tratamento de evento
+        b.setOnClickListener(v -> {
+            Button b=(Button) v;
+            b.setText(Integer.toString(i));
+            i++;
+
         });
     }
-
     @Override
     protected void onStart() {
         super.onStart();
         Log.d("ciclo_vida", "onStart");
     }
-
     @Override
     protected void onResume() {
         super.onResume();
         Log.d("ciclo_vida" ,"onResume");
     }
-
     @Override
     protected void onPause() {
         super.onPause( );
         Log.d("ciclo_vida" ,"onPause");
-
-
-
     }
 }

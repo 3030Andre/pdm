02cc4825ef235276cd7df6b076be3fdd9d493517
Your Name <you@example.com>
2025-10-01 15:02:33 -0300

    sorteio

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.app;
 
+
+
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,24 +15,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     TextView tv;
     Button b;
     int i =0;
+    EditText edmin, edmax;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         b=findViewById(R.id.button);
-        tv=findViewById(R.id.textView);
-        tv.setText("blá");
-        b.setText("sorteardxcfvgbhnm");
+        tv=findViewById(R.id.tvresultado);
+        edmin = findViewById(R.id.edmin);
+        edmax = findViewById (R.id.edmax);
+
+
+
        //tratamento de evento
         b.setOnClickListener(v -> {
-            Button b=(Button) v;
-            b.setText(Integer.toString(i));
-            i++;
+            Integer minimo = Integer.parseInt(edmin.getText().toString());
+            Integer maximo = Integer.parseInt(edmax.getText().toString());
+            Random random= new Random();
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+                int r=random.nextInt(minimo, maximo);
+                tv.setText(Integer.toString(r));
+            }
+
 
         });
     }
